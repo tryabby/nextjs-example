a37ca9f9a966a0b3385ea15794014495dae38c30
Tim <tim.raderschad@gmail.com>
2023-04-21 14:04:30 +0200

    init repo

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import type { AppProps } from 'next/app'
+import type { LayoutProps } from '@vercel/examples-ui/layout'
+import { getLayout } from '@vercel/examples-ui'
+import { abby } from '@lib/ab-testing'
+import '@vercel/examples-ui/globals.css'
+import { ABBY_DATA_KEY } from '@tryabby/next'
+
+function MyApp({ Component, pageProps: { [ABBY_DATA_KEY]: abbyData, ...pageProps } }: AppProps) {
+  const Layout = getLayout<LayoutProps>(Component)
+
+  return (
+    <abby.AbbyProvider initialData={abbyData}>
+      <Layout
+        title="AB Testing with A/BBY"
+        path="edge-middleware/abby-ab-testing"
+      >
+        <Component {...pageProps} />
+      </Layout>
+    </abby.AbbyProvider>
+  )
+}
+
+
+export default abby.withAbby(MyApp)
